cbcbc2571b1fb066fa6a47db93b8d43b04a207d6
kapitanluffy <kapitanluffy@gmail.com>
2023-10-09 22:58:08 +0800

    Cleanup run_command and remove -e option in ripgrep call

--- src/utils.py
+++ src/utils.py
@@ -1,72 +1,10 @@
 import subprocess
-import select
-import sys
-import threading
-from typing import Generator, Literal
 from .Terminal import FastFuzzyFinder
-
-# read_fd, write_fd = os.pipe()
-# python -c "print('hello!');x=input('Enter something: ');print(x)"
-# python -c "print('hello!');x=input('Enter something: ');print('foo');print(x);"
-
-STYLE = """
-html {{
-    border-left-style:solid;
-    border-left-color:#111111;
-    border-left-width:0.1px;
-}}
-"""
-
-HTML = "<body><style>%s</style>{body}</body>" % STYLE
-
-
-def output_sheet():
-    print("output sheet thread!")
-    while True:
-        if FastFuzzyFinder.process.stdout is None:
-            print("bb")
-            continue
-
-        line = FastFuzzyFinder.process.stdout.readline()
-
-        if not line:
-            print("no output lines")
-            break
-
-        if FastFuzzyFinder.sheet is None:
-            print("sheet is gone")
-            break
-
-        # if output != "" and isinstance(output, int) is False:
-        #     # output = output.replace(" ", sublime.html.entities.html5.get('nbsp'))
-        #     # line = "<div style=\"margin-bottom:1rem;\">%s</div>" % sublime.html.escape(output)
-        #     line = "<div style=\"margin-bottom:1rem;\">%s</div>" % output
-        # elif output != "" and isinstance(output, int):
-        #     line = "<div style=\"margin-bottom:1rem;\">Return Code: %s</div>" % output
-        # else:
-        #     line = "<div style=\"margin-bottom:1rem;\">&nbsp;</div>"
-
-        print("> ", line)
-        FastFuzzyFinder.thread_output.put([line, 'ok'])
-        # content = HTML.format(body=' '.join(Terminal.output))
-        # Terminal.sheet.set_contents(content)
-    print("output sheet done!")
-
-
-def append_sheet_content(output: str):
-    line = "<div style=\"margin-bottom:1rem;\">%s</div>" % output
-    FastFuzzyFinder.output.append(line)
-    content = HTML.format(body=' '.join(FastFuzzyFinder.output))
-    FastFuzzyFinder.sheet.set_contents(content)
 
 
 def run_command(command: str, directory: str, output, input):
     try:
-        # cmd = ["d:\\~\\bin\\ripgrep\\rg.exe", "--smart-case", "--column", "--no-heading", command]
-        # cmd = "d:\\~\\bin\\ripgrep\\rg.exe plugin"
-        # print("cwd", "cd \"%s\" && d:/~/bin/fzf.exe" % directory)
-
-        cmd = "rg --column --no-heading --smart-case -e \"%s\" ./" % command
+        cmd = "rg --column --no-heading --smart-case %s ./" % command
         FastFuzzyFinder.process = subprocess.Popen(
             cmd,
             shell=True,
